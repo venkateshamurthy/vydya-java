85923f9b9cbb550bef0564bf58c8e08e04cd4a7f
vmurthy <venkateshamurthy@users.noreply.github.com>
2025-03-21 22:40:22 +0530

    Update SelectionSort.java

--- src/vydya/algos/SelectionSort.java
+++ src/vydya/algos/SelectionSort.java
@@ -1,46 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package vydya.algos;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
+/**
+ *
+ * @author venkatm
+ */
 public class SelectionSort {
-    static Random rand = new Random(10L);
+    static final  Random rand = new Random(10L);
+    final int[] data;
 
-    public static void main(String[] args) {
-        int[] array = new int[rand.nextInt(10, 20)];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = rand.nextInt(10, 10000);
-        }
-
-        SelectionSort selectionSort = new SelectionSort();
-
-        System.out.println("The input array is: "+ Arrays.toString(array));
-        long startTime = System.nanoTime();
-        selectionSort.selectionSort(array);
-        long endTime = System.nanoTime();
-        System.out.println("Selection Sorted array: " + Arrays.toString(array)
-                + "\nDuration: " + (endTime - startTime) + " ns");
+    public SelectionSort(int[] input) {
+        data = input;
     }
-
-
-    public void selectionSort(int[] a) {
-        for (int i = 0; i < a.length - 1; i++) {
+    
+   
+    public void sort(int start, int end) {
+        for (int i = start; i< end - 1; i++){
             int minIndex = i;
-            int min = a[minIndex];
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[j] < min) {
-                    min = a[j];
+            for (int j = i + 1; j<=end; j++){
+                if (data[j] < data[minIndex]) {
                     minIndex = j;
                 }
             }
-            swap(a, i, minIndex);
+            swap(i, minIndex);
         }
     }
-
-    static void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
+    
+    public static void main(String[] args) {
+        System.out.println("\nRuning Selection Sort...");
+        int[] input = createRandomArray();
+        SelectionSort sorter = new SelectionSort(input);
+        sorter.sort(0, input.length - 1);
+        System.out.println("Sorted Output :"+Arrays.toString(input));
     }
+    
+    static int[] createRandomArray() {
+        int[] input = new int[rand.nextInt(10, 20)];
+        for (int i = 0; i < input.length; i++) input[i] = rand.nextInt(100, 1000);
+        System.out.println("Unsorted Input:" + Arrays.toString(input));
+        return input;
+    }
+    
+     void swap(int a, int b) {
+        int temp = data[a];
+        data[a] = data[b];
+        data[b] = temp;
+    }
+    
 }
